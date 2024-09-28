aa0f75ea78f2acc143f0ce62debc5e28b6401e96
unknown <sebastian-olthoff@gmx.de>
2024-09-28 18:48:01 +0200

    added rss feed route to solve cors problems

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "./db/index.js";
 import cors from "cors";
 import "./models/index.js";
+import rssFeedRouter from './routes/rssFeedRoute.js';
 import userRoute from "./routes/usersRoute.js";
 import roleRoute from "./routes/rolesRoute.js";
 import reviewRouter from "./routes/reviewsRoute.js";
@@ -43,6 +44,7 @@ app.use("/barriers", barrierRouter);
 app.use("/file-upload", FileRouter);
 app.use("/profilePhotos", ProfilePhotosRouter);
 app.use("/auth", authRouter);
+app.use('/rss', rssFeedRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
