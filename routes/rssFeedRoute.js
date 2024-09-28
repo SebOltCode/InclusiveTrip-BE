766726e06debe0640eec8d0372fe7adbe3a6b652
unknown <sebastian-olthoff@gmx.de>
2024-09-28 19:27:26 +0200

    xml_to_json

--- routes/rssFeedRoute.js
+++ routes/rssFeedRoute.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import axios from 'axios';
+import { parseString } from 'xml2js';
 
 const rssFeedRouter = Router();
 
@@ -7,7 +8,22 @@ rssFeedRouter.get('/rss-feed', async (req, res) => {
     try {
         const feedUrl = 'https://www.barrierefrei-aufgerollt.at/feed/';
         const response = await axios.get(feedUrl);
-        res.json({ feed: response.data });
+
+        parseString(response.data, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: 'Fehler beim Parsen des RSS-Feeds' });
+            }
+
+            const feedItems = result.rss.channel[0].item.map(item => ({
+                title: item.title[0],
+                link: item.link[0],
+                description: item.description[0],
+                pubDate: item.pubDate[0],
+                author: item['dc:creator'] ? item['dc:creator'][0] : null,
+            }));
+
+            res.json({ feed: feedItems });
+        });
     } catch (error) {
         console.error('Fehler beim Abrufen des RSS-Feeds:', error);
         res.status(500).json({ error: 'Fehler beim Abrufen des RSS-Feeds' });
